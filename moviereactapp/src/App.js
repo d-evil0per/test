7ac0796fd66fffc29253550c482af5a6bc2e1acf
D-eviloper <dhir.xcess@gmail.com>
2022-11-27 12:07:59 +0530

    Add files via upload

---
+++ moviereactapp/src/App.js
@@ -0,0 +1,127 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import SearchIcon from '@mui/icons-material/Search';
+import MovieCard from './components/MovieCard/MovieCard';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+
+
+
+
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isFav, setIsFav] = useState(false);
+  const [currPage, setCurrPage] = useState('');
+  // const [value, setValue]=useState('');
+
+  const getMovie = async (type) => {
+    var url;
+    switch (type) {
+      case "movies-coming":
+        url = `http://localhost:3000/movies-coming`;
+        setCurrPage('movies-coming')
+        break;
+
+      case "movies-in-theaters":
+        url = `http://localhost:3000/movies-in-theaters`;
+        setCurrPage('movies-in-theaters')
+        break;
+
+      case "top-rated-india":
+        url = `http://localhost:3000/top-rated-india`;
+        setCurrPage('top-rated-india')
+        break;
+      case "top-rated-movies":
+        url = `http://localhost:3000/top-rated-movies`;
+        setCurrPage('top-rated-movies')
+        break;
+      case "favourite":
+        url = `http://localhost:3000/favourite`;
+        setCurrPage('favourite')
+        break;
+      default:
+        break;
+    }
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    if (responseJson) {
+      setMovies(responseJson);
+    }
+  };
+
+  const search = (value) => {
+    var results = [];
+    var searchField = "title";
+    var searchVal = value;
+    for (var i = 0; i < movies.length; i++) {
+      if (movies[i][searchField].toLowerCase().indexOf(searchVal) != -1) {
+        results.push(movies[i]);
+      }
+    }
+
+    setMovies(results);
+
+
+  }
+
+  const handlefav = (movie) => {
+    const url = `http://localhost:3000/favourite`;
+    fetch(url, {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(movie),
+    })
+      .then(res => res.json())
+      .then(
+        data => setMovies(prevState => [...prevState, data]),
+        error => console.log(error.message)
+      );
+    toast.success("Added to favourite");
+    setIsFav(true);
+  }
+  const handleRemove=(movie) => {
+    const url = `http://localhost:3000/favourite`;
+    const response = await fetch(url);
+    const responseJson = await response.json();
+ 
+  }
+
+  useEffect(() => {
+    getMovie("movies-coming");
+  }, []);
+  return (
+    <div className="App">
+      <Navbar className='navbar'>
+        <Container fluid style={{background:'white'}}>
+          <Nav className="me-auto" style={{gap:'5px',padding:'6px 0 0 0'}} c>
+            <Nav.Link className='navlink' onClick={() => { getMovie("movies-coming") }} >Movies in Theater</Nav.Link>
+            <Nav.Link className='navlink' onClick={() => { getMovie("movies-in-theaters") }} >Coming Soon</Nav.Link>
+            <Nav.Link className='navlink' onClick={() => { getMovie("top-rated-india") }}>Top rated Indian</Nav.Link>
+            <Nav.Link className='navlink' onClick={() => { getMovie("top-rated-movies") }}>Top rated movies</Nav.Link>
+            <Nav.Link className='navlink' onClick={() => { getMovie("favourite") }}>Favourites</Nav.Link>
+          </Nav>
+          <Form style={{ marginRight: '2px' }}>
+            <Form.Group className="mr-5">
+
+              <Form.Control onChange={(e) => search(e.target.value)} type="email" placeholder="Search Movie" />
+
+            </Form.Group>
+          </Form>
+          <SearchIcon style={{justifyContent:'center'}} color="primary" />
+        </Container>
+      </Navbar>
+      <ToastContainer />
+      <MovieCard movies={movies} handlefav={handlefav} isFav={isFav}  currPage ={currPage}/>
+
+
+    </div>
+  );
+}
+
+export default App;
