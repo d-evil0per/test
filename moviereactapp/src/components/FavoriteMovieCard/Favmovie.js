8bfe6e0593ced293c8dfc2bd84c4b00f6fed16db
D-eviloper <dhir.xcess@gmail.com>
2022-11-27 23:30:20 +0530

    Delete moviereactapp directory

--- moviereactapp/src/components/FavoriteMovieCard/Favmovie.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import MovieCard from "../MovieCard/MovieCard";
-
-const FavMovie =(props) =>{
-    return(
-        <p>Add to Favourites <FavoriteIcon fontSize="small" sx={{ color: pink[500]}} /></p>
-    )
-}
