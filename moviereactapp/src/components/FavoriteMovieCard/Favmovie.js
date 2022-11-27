e1fd808b6efd40c1bcc489a83012439f3769ed02
D-eviloper <dhir.xcess@gmail.com>
2022-11-27 23:32:13 +0530

    Add files via upload

---
+++ moviereactapp/src/components/FavoriteMovieCard/Favmovie.js
@@ -0,0 +1,8 @@
+import React from "react";
+import MovieCard from "../MovieCard/MovieCard";
+
+const FavMovie =(props) =>{
+    return(
+        <p>Add to Favourites <FavoriteIcon fontSize="small" sx={{ color: pink[500]}} /></p>
+    )
+}
